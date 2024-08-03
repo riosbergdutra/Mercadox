1de10a39532955e72de530a6524e4c13ebd71d4b
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-03 15:40:02 -0300

    feat: arquivo sql

---
+++ usuario/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- Criação da tabela 'usuarios'
+CREATE TABLE usuarios (
+    id_usuario UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    roles VARCHAR(255)[] NOT NULL,
+    data_conta DATE,
+);
