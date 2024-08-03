e16af227c0c1f992ce9def1f45c06816567e4a21
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-03 15:45:33 -0300

    feat: repository do usuario

---
+++ usuario/src/main/java/user/api/usuario/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package user.api.usuario.usuario.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import user.api.usuario.usuario.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    <Optional>Usuario findByEmail(String email);
+}
