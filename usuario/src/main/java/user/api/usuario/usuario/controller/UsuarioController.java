7e6af39c3e4718242f8ee1376f3e263dd650e447
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-08 16:35:03 -0300

    controller do usuario

--- usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
@@ -1,54 +1,56 @@
 package user.api.usuario.usuario.controller;
 
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import user.api.usuario.usuario.dtos.LoginRequest;
+import user.api.usuario.usuario.dtos.AcharUsuarioIdDto.UsuarioIdResponseDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioRequestDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioResponseDto;
+import user.api.usuario.usuario.enums.Role;
 import user.api.usuario.usuario.model.Usuario;
-import user.api.usuario.usuario.service.impl.UsuarioServiceImpl;
+import user.api.usuario.usuario.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
-    private UsuarioServiceImpl usuarioService;
+    private UsuarioService usuarioService;
 
-    @PostMapping("/criar")
-    public ResponseEntity<UsuarioResponseDto> criarUsuario(@RequestBody UsuarioRequestDto usuarioDto) {
-        try {
-            UsuarioResponseDto resposta = usuarioService.saveUsuario(usuarioDto);
-            return new ResponseEntity<>(resposta, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST); 
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR); 
-        }
+    @PostMapping(value = "/criar")
+    public ResponseEntity<UsuarioResponseDto> criarUsuario(
+            @RequestParam("nome") String nome,
+            @RequestParam("email") String email,
+            @RequestParam("senha") String senha,
+            @RequestParam("role") Role role,
+            @RequestParam(value = "foto", required = false) MultipartFile foto) {
+
+        UsuarioRequestDto usuarioDto = new UsuarioRequestDto(nome, foto, email, senha, role);
+        UsuarioResponseDto resposta = usuarioService.saveUsuario(usuarioDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUsuarioById(@PathVariable UUID id) {
-        Usuario usuario = usuarioService.getUsuarioById(id);
-        if (usuario != null) {
-            return new ResponseEntity<>(usuario, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<UsuarioIdResponseDto> getUsuarioById(@PathVariable UUID id) {
+        UsuarioIdResponseDto usuario = usuarioService.getUsuarioById(id);
+        return ResponseEntity.ok(usuario);
     }
 
     @PostMapping("/email")
     public ResponseEntity<Usuario> getUsuarioByEmail(@RequestBody LoginRequest loginRequest) {
         Usuario usuario = usuarioService.getUsuarioByEmail(loginRequest.email(), loginRequest.senha());
-        if (usuario != null) {
-            return ResponseEntity.ok(usuario);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 401 Unauthorized
-        }
+        return ResponseEntity.ok(usuario);
     }
-
 }
