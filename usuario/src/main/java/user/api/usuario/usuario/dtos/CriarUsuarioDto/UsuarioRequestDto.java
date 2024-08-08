aaf1c0082bbc5cc7ee432986bd785b602953611c
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-08 16:33:25 -0300

    requisicao para criar o usuario

--- usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
+++ usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
@@ -1,18 +1,17 @@
 package user.api.usuario.usuario.dtos.CriarUsuarioDto;
 
-import java.time.LocalDate;
-import java.util.Set;
+import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import user.api.usuario.usuario.enums.Role;
 
-
-
 public record UsuarioRequestDto(
-        String nome,
-        byte[] imagem,
-        String email,
-        String senha,
-        Set<Role> roles,
-        LocalDate dataConta) {
+                @NotBlank(message = "Nome não pode estar vazio") String nome,
+                MultipartFile imagem,
+                @NotBlank(message = "Email não pode estar vazio") @Email(message = "Email inválido") String email,
+                @NotBlank(message = "Senha não pode estar vazia") String senha,
+                @NotNull(message = "Role não pode estar vazia") Role role) {
 
 }
