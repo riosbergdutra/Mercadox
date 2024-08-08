670575c9390d1e5969c8ba5fef8e67660ddfa4eb
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-08 16:34:35 -0300

    service para s3

--- usuario/src/main/java/user/api/usuario/usuario/service/S3Service.java
+++ usuario/src/main/java/user/api/usuario/usuario/service/S3Service.java
@@ -1,11 +1,15 @@
 package user.api.usuario.usuario.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.core.sync.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
+
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @Service
 public class S3Service {
@@ -13,28 +17,28 @@ public class S3Service {
     @Autowired
     private S3Client s3Client;
 
-    private final String bucketName = "FotoPerfil"; // Substitua pelo nome do seu bucket
+    private static final String BUCKET_NAME = "usuario";
 
-   
-    public String uploadImage(String key, byte[] imageContent) {
-        try {
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(key)
-                    .build();
+    public String uploadImagemS3(String key, MultipartFile imagem) throws IOException {
+        File file = convertMultipartFileToFile(imagem);
 
-            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(imageContent));
-            System.out.println("Imagem carregada com chave: " + key); // Log da chave
-            return key; // Retorna o nome da imagem para armazenamento no banco de dados
-        } catch (S3Exception e) {
-            e.printStackTrace(); // Tratar exceções de forma adequada
-            throw new RuntimeException("Erro ao fazer upload da imagem para o S3", e);
-        }
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+            .bucket(BUCKET_NAME)
+            .key(key)
+            .build();
+
+        s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromFile(file));
+        file.delete();
+
+        // Construir a URL da imagem no S3
+        return String.format("http://localhost:4566/%s/%s", BUCKET_NAME, key);
     }
 
-    public String getImageUrl(String key) {
-        // Cria a URL completa do objeto no S3
-        return String.format("https://%s.s3.amazonaws.com/%s", bucketName, key);
+    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convFile)) {
+            fos.write(file.getBytes());
+        }
+        return convFile;
     }
 }
-
