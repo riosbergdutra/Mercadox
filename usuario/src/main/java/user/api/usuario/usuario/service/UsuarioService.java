524a12146d2d33d179f32319c052394fa0317d8f
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-14 17:48:07 -0300

    adicionando anotacoes e modificando os metodos

--- usuario/src/main/java/user/api/usuario/usuario/service/UsuarioService.java
+++ usuario/src/main/java/user/api/usuario/usuario/service/UsuarioService.java
@@ -5,12 +5,19 @@ import java.util.UUID;
 import user.api.usuario.usuario.dtos.AcharUsuarioIdDto.UsuarioIdResponseDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioRequestDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioResponseDto;
+import user.api.usuario.usuario.dtos.MudarSenha.MudarSenhaRequest;
 import user.api.usuario.usuario.model.Usuario;
 
 public interface UsuarioService {
+    // metodo para criar uma conta do usuario, sendo admin, usuario ou vendedor
     public UsuarioResponseDto saveUsuario(UsuarioRequestDto usuarioDto);
 
-    public UsuarioIdResponseDto getUsuarioById(UUID id);
+    // achar o usuario pelo id dele (precisa ser autenticado)
+    public UsuarioIdResponseDto getUsuarioById(UUID id, UUID userId);
 
+    // metodo na qual o autenticador vai se comunicar via http para criar o token
     public Usuario getUsuarioByEmail(String email, String senha);
+
+    // permite o usuario mudar a senha (precisa ser autenticado)
+    public String mudarSenha(UUID id, UUID userId, MudarSenhaRequest mudarSenhaRequest);
 }
