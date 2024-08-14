640689db594c30e2b0c9fa4290a75d4fd33edda5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-14 17:49:30 -0300

    modificando mapping e adicionando mudarsenha no controlador

--- usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
@@ -8,28 +8,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.core.Authentication;
 
 import user.api.usuario.usuario.dtos.LoginRequest;
 import user.api.usuario.usuario.dtos.AcharUsuarioIdDto.UsuarioIdResponseDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioRequestDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioResponseDto;
+import user.api.usuario.usuario.dtos.MudarSenha.MudarSenhaRequest;
 import user.api.usuario.usuario.enums.Role;
 import user.api.usuario.usuario.model.Usuario;
 import user.api.usuario.usuario.service.UsuarioService;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
-    @PostMapping(value = "/criar")
+    @PostMapping("/criar")
     public ResponseEntity<UsuarioResponseDto> criarUsuario(
             @RequestParam("nome") String nome,
             @RequestParam("email") String email,
@@ -43,8 +46,9 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioIdResponseDto> getUsuarioById(@PathVariable UUID id) {
-        UsuarioIdResponseDto usuario = usuarioService.getUsuarioById(id);
+    public ResponseEntity<UsuarioIdResponseDto> getUsuarioById(@PathVariable UUID id, Authentication authentication) {
+        UUID userId = UUID.fromString(authentication.getName());
+        UsuarioIdResponseDto usuario = usuarioService.getUsuarioById(id, userId);
         return ResponseEntity.ok(usuario);
     }
 
@@ -52,5 +56,17 @@ public class UsuarioController {
     public ResponseEntity<Usuario> getUsuarioByEmail(@RequestBody LoginRequest loginRequest) {
         Usuario usuario = usuarioService.getUsuarioByEmail(loginRequest.email(), loginRequest.senha());
         return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/senha/{id}") // Use PUT para alterações de recurso
+    public ResponseEntity<String> mudarSenha(
+            @PathVariable UUID id,
+            Authentication authentication,
+            @RequestBody MudarSenhaRequest mudarSenhaRequest) {
+
+        UUID userId = UUID.fromString(authentication.getName());
+        String resultado = usuarioService.mudarSenha(id, userId, mudarSenhaRequest);
+        return ResponseEntity.ok(resultado);
+
     }
 }
