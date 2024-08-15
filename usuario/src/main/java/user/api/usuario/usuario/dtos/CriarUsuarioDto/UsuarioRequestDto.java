474172ece47eb324b3e0b2c965ccc0265cb338ec
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-15 18:26:20 -0300

    organizando codigo

--- usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
+++ usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
@@ -8,10 +8,9 @@ import jakarta.validation.constraints.NotNull;
 import user.api.usuario.usuario.enums.Role;
 
 public record UsuarioRequestDto(
-                @NotBlank(message = "Nome não pode estar vazio") String nome,
-                MultipartFile imagem,
-                @NotBlank(message = "Email não pode estar vazio") @Email(message = "Email inválido") String email,
-                @NotBlank(message = "Senha não pode estar vazia") String senha,
-                @NotNull(message = "Role não pode estar vazia") Role role) {
-
+        @NotBlank(message = "Nome não pode estar vazio") String nome,
+        MultipartFile imagem,
+        @NotBlank(message = "Email não pode estar vazio") @Email(message = "Email inválido") String email,
+        @NotBlank(message = "Senha não pode estar vazia") String senha,
+        @NotNull(message = "Role não pode estar vazia") Role role) {
 }
