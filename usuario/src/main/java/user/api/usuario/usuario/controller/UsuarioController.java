58bee9591537df72a185244ba94673c288bc7c18
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-15 18:25:55 -0300

    garantindo a seguranca do criar usuario e transofrmando o post em get

--- usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,15 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.security.core.Authentication;
+import jakarta.validation.Valid;
 
-import user.api.usuario.usuario.dtos.LoginRequest;
+import org.springframework.security.core.Authentication;
 import user.api.usuario.usuario.dtos.AcharUsuarioIdDto.UsuarioIdResponseDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioRequestDto;
 import user.api.usuario.usuario.dtos.CriarUsuarioDto.UsuarioResponseDto;
 import user.api.usuario.usuario.dtos.MudarSenha.MudarSenhaRequest;
-import user.api.usuario.usuario.enums.Role;
 import user.api.usuario.usuario.model.Usuario;
 import user.api.usuario.usuario.service.UsuarioService;
 
@@ -33,18 +32,12 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping("/criar")
-    public ResponseEntity<UsuarioResponseDto> criarUsuario(
-            @RequestParam("nome") String nome,
-            @RequestParam("email") String email,
-            @RequestParam("senha") String senha,
-            @RequestParam("role") Role role,
-            @RequestParam(value = "foto", required = false) MultipartFile foto) {
+public ResponseEntity<UsuarioResponseDto> criarUsuario(
+        @Valid @ModelAttribute UsuarioRequestDto usuarioDto) {
 
-        UsuarioRequestDto usuarioDto = new UsuarioRequestDto(nome, foto, email, senha, role);
-        UsuarioResponseDto resposta = usuarioService.saveUsuario(usuarioDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
-    }
-
+    UsuarioResponseDto resposta = usuarioService.saveUsuario(usuarioDto);
+    return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+}
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioIdResponseDto> getUsuarioById(@PathVariable UUID id, Authentication authentication) {
         UUID userId = UUID.fromString(authentication.getName());
@@ -52,11 +45,13 @@ public class UsuarioController {
         return ResponseEntity.ok(usuario);
     }
 
-    @PostMapping("/email")
-    public ResponseEntity<Usuario> getUsuarioByEmail(@RequestBody LoginRequest loginRequest) {
-        Usuario usuario = usuarioService.getUsuarioByEmail(loginRequest.email(), loginRequest.senha());
+    @GetMapping("/email/")
+    public ResponseEntity<Usuario> getUsuarioByEmail(@RequestParam String email, @RequestParam String senha) {
+        Usuario usuario = usuarioService.getUsuarioByEmail(email, senha);
         return ResponseEntity.ok(usuario);
     }
+    
+    
 
     @PutMapping("/senha/{id}") // Use PUT para alterações de recurso
     public ResponseEntity<String> mudarSenha(
