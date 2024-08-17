fee51d315d8b6d2111a1747bdfa7a1674112a5be
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-17 17:38:07 -0300

    adicionando delete e documentacao

--- usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/user/api/usuario/usuario/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,11 @@ import user.api.usuario.usuario.dtos.MudarSenha.MudarSenhaRequest;
 import user.api.usuario.usuario.model.Usuario;
 import user.api.usuario.usuario.service.UsuarioService;
 
+/**
+ * Controlador para gerenciar operações relacionadas aos usuários.
+ * 
+ * Fornece endpoints para criar, consultar, atualizar e excluir usuários.
+ */
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -31,13 +37,27 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    /**
+     * Endpoint para criar um novo usuário.
+     * 
+     * @param usuarioDto DTO contendo os dados do usuário a ser criado.
+     * @return ResponseEntity com o DTO do usuário criado e status 201 (CREATED).
+     */
     @PostMapping("/criar")
-public ResponseEntity<UsuarioResponseDto> criarUsuario(
-        @Valid @ModelAttribute UsuarioRequestDto usuarioDto) {
+    public ResponseEntity<UsuarioResponseDto> criarUsuario(
+            @Valid @ModelAttribute UsuarioRequestDto usuarioDto) {
 
-    UsuarioResponseDto resposta = usuarioService.saveUsuario(usuarioDto);
-    return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
-}
+        UsuarioResponseDto resposta = usuarioService.saveUsuario(usuarioDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
+
+    /**
+     * Endpoint para obter detalhes de um usuário por ID.
+     * 
+     * @param id             ID do usuário a ser consultado.
+     * @param authentication Objeto de autenticação contendo o ID do usuário logado.
+     * @return ResponseEntity com o DTO do usuário e status 200 (OK).
+     */
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioIdResponseDto> getUsuarioById(@PathVariable UUID id, Authentication authentication) {
         UUID userId = UUID.fromString(authentication.getName());
@@ -45,14 +65,28 @@ public ResponseEntity<UsuarioResponseDto> criarUsuario(
         return ResponseEntity.ok(usuario);
     }
 
+    /**
+     * Endpoint para obter um usuário por email e senha.
+     * 
+     * @param email Email do usuário a ser consultado.
+     * @param senha Senha do usuário a ser consultado.
+     * @return ResponseEntity com o objeto Usuario e status 200 (OK).
+     */
     @GetMapping("/email/")
     public ResponseEntity<Usuario> getUsuarioByEmail(@RequestParam String email, @RequestParam String senha) {
         Usuario usuario = usuarioService.getUsuarioByEmail(email, senha);
         return ResponseEntity.ok(usuario);
     }
-    
-    
 
+    /**
+     * Endpoint para alterar a senha do usuário.
+     * 
+     * @param id                ID do usuário cuja senha será alterada.
+     * @param authentication    Objeto de autenticação contendo o ID do usuário
+     *                          logado.
+     * @param mudarSenhaRequest DTO com as informações para alterar a senha.
+     * @return ResponseEntity com uma mensagem de sucesso e status 200 (OK).
+     */
     @PutMapping("/senha/{id}") // Use PUT para alterações de recurso
     public ResponseEntity<String> mudarSenha(
             @PathVariable UUID id,
@@ -62,6 +96,19 @@ public ResponseEntity<UsuarioResponseDto> criarUsuario(
         UUID userId = UUID.fromString(authentication.getName());
         String resultado = usuarioService.mudarSenha(id, userId, mudarSenhaRequest);
         return ResponseEntity.ok(resultado);
+    }
 
+    /**
+     * Endpoint para excluir um usuário.
+     * 
+     * @param id             ID do usuário a ser excluído.
+     * @param authentication Objeto de autenticação contendo o ID do usuário logado.
+     * @return ResponseEntity com uma mensagem de sucesso e status 200 (OK).
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUsuario(@PathVariable UUID id, Authentication authentication) {
+        UUID userId = UUID.fromString(authentication.getName());
+        String resultado = usuarioService.deleteUsuario(id, userId);
+        return ResponseEntity.ok(resultado);
     }
 }
