872cf385b3beaec3b8eeab1bd69ed3d76b93b3ce
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-17 17:41:01 -0300

    novo metodo para deletar usuario e documentacao no impl

--- usuario/src/main/java/user/api/usuario/usuario/service/impl/UsuarioServiceImpl.java
+++ usuario/src/main/java/user/api/usuario/usuario/service/impl/UsuarioServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import user.api.usuario.usuario.dtos.AcharUsuarioIdDto.UsuarioIdResponseDto;
@@ -23,6 +22,10 @@ import user.api.usuario.usuario.service.UsuarioService;
 import user.api.usuario.usuario.exceptions.UsuarioNotFoundException;
 import user.api.usuario.usuario.exceptions.InvalidCredentialsException;
 
+/**
+ * Implementação do serviço de usuário, responsável por gerenciar operações
+ * relacionadas a usuários.
+ */
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 
@@ -38,8 +41,14 @@ public class UsuarioServiceImpl implements UsuarioService {
     @Autowired
     private SqsTemplate sqsTemplate;
 
+    /**
+     * Salva um novo usuário no sistema.
+     *
+     * @param usuarioDto Dados do usuário a ser criado.
+     * @return Dados do usuário criado.
+     */
     @Override
-    public UsuarioResponseDto saveUsuario(@ModelAttribute UsuarioRequestDto usuarioDto) {
+    public UsuarioResponseDto saveUsuario(UsuarioRequestDto usuarioDto) {
         Usuario novoUsuario = new Usuario();
         novoUsuario.setIdUsuario(UUID.randomUUID());
         novoUsuario.setNome(usuarioDto.nome());
@@ -64,25 +73,20 @@ public class UsuarioServiceImpl implements UsuarioService {
                     });
         }
 
-        // Salva o novo usuário
-        Usuario usuarioCriado = usuarioRepository.save(novoUsuario);
-
-        // Verifica se o usuário foi salvo com sucesso antes de enviar a mensagem
-        if (usuarioCriado != null) {
-            String queueUrl = "http://localhost:4566/000000000000/usuarios";
-            String messageBody = String.format("idUsuario: %s\nemail: %s\nsenha: %s",
-                    usuarioCriado.getIdUsuario(), usuarioCriado.getEmail(), usuarioCriado.getSenha());
-
-            // Envia a mensagem para a fila SQS
-            sqsTemplate.send(queueUrl, messageBody);
-        } else {
-            throw new RuntimeException("failed.save.user");
-        }
+        // Salva o novo usuário e obtém o usuário criado
+        usuarioRepository.save(novoUsuario);
 
         // Retorna a resposta com os dados do usuário criado
         return new UsuarioResponseDto(novoUsuario.getNome(), novoUsuario.getEmail(), novoUsuario.getImagem());
     }
 
+    /**
+     * Recupera um usuário pelo ID e pelo userId.
+     *
+     * @param id     ID do usuário a ser recuperado.
+     * @param userId ID do usuário autenticado.
+     * @return Dados do usuário encontrado.
+     */
     @Override
     public UsuarioIdResponseDto getUsuarioById(UUID id, UUID userId) {
         return usuarioRepository.findById(id)
@@ -96,6 +100,13 @@ public class UsuarioServiceImpl implements UsuarioService {
                 .orElseThrow(() -> new UsuarioNotFoundException("user.not.found"));
     }
 
+    /**
+     * Recupera um usuário pelo email e senha.
+     *
+     * @param email Email do usuário.
+     * @param senha Senha do usuário.
+     * @return Usuário correspondente às credenciais fornecidas.
+     */
     @Override
     public Usuario getUsuarioByEmail(String email, String senha) {
         return Optional.ofNullable(usuarioRepository.findByEmail(email))
@@ -103,17 +114,50 @@ public class UsuarioServiceImpl implements UsuarioService {
                 .orElseThrow(() -> new InvalidCredentialsException("invalid.credentials"));
     }
 
+    /**
+     * Altera a senha do usuário.
+     *
+     * @param id                ID do usuário.
+     * @param userId            ID do usuário autenticado.
+     * @param mudarSenhaRequest Dados para a mudança de senha.
+     * @return Mensagem de sucesso se a senha for alterada com sucesso.
+     */
     @Override
     public String mudarSenha(UUID id, UUID userId, MudarSenhaRequest mudarSenhaRequest) {
         return usuarioRepository.findById(id)
                 .filter(u -> id.equals(userId))
                 .map(u -> {
                     if (!passwordEncoder.matches(mudarSenhaRequest.SenhaAntiga(), u.getSenha())) {
-                        throw new InvalidCredentialsException("Senha antiga incorreta");
+                        throw new InvalidCredentialsException("password.not.sucess");
                     }
                     u.setSenha(passwordEncoder.encode(mudarSenhaRequest.SenhaNova()));
                     usuarioRepository.save(u);
                     return "password.success";
+                })
+                .orElseThrow(() -> new UsuarioNotFoundException("user.not.found"));
+    }
+
+    /**
+     * Exclui um usuário do sistema.
+     *
+     * @param id     ID do usuário a ser excluído.
+     * @param userId ID do usuário autenticado.
+     * @return Mensagem de sucesso se o usuário for excluído com sucesso.
+     */
+    @Override
+    public String deleteUsuario(UUID id, UUID userId) {
+        return usuarioRepository.findById(id)
+                .filter(usuario -> id.equals(userId))
+                .map(usuario -> {
+                    // Exclui o usuário do banco de dados
+                    usuarioRepository.delete(usuario);
+
+                    // Envia uma mensagem para a fila SQS
+                    String queueUrl = "http://localhost:4566/000000000000/usuario";
+                    String messageBody = "Usuário atualizado pelo ID: " + id;
+                    sqsTemplate.send(queueUrl, messageBody);
+
+                    return "user.deleted.success";
                 })
                 .orElseThrow(() -> new UsuarioNotFoundException("user.not.found"));
     }
