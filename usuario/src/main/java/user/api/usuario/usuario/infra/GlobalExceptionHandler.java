101ce0d2611d9f944b57b6cadf6aa792c49368d7
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-17 17:38:35 -0300

    adicionando documentacao para a global exception

--- usuario/src/main/java/user/api/usuario/usuario/infra/GlobalExceptionHandler.java
+++ usuario/src/main/java/user/api/usuario/usuario/infra/GlobalExceptionHandler.java
@@ -11,9 +11,20 @@ import user.api.usuario.usuario.exceptions.InvalidCredentialsException;
 import user.api.usuario.usuario.exceptions.UsuarioNotFoundException;
 import org.springframework.http.HttpStatus;
 
+/**
+ * Classe de manipulação global de exceções.
+ * 
+ * Esta classe fornece manipuladores para exceções específicas e retorna respostas apropriadas para o cliente.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Manipulador para exceções do tipo UsuarioNotFoundException.
+     * 
+     * @param ex Exceção lançada quando um usuário não é encontrado.
+     * @return Resposta com status 404 (Not Found) e mensagem de erro.
+     */
     @ExceptionHandler(UsuarioNotFoundException.class) 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleUsuarioNotFound(UsuarioNotFoundException ex) {
@@ -21,6 +32,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    /**
+     * Manipulador para exceções do tipo ImageProcessingException.
+     * 
+     * @param ex Exceção lançada durante o processamento de uma imagem.
+     * @return Resposta com status 400 (Bad Request) e mensagem de erro.
+     */
     @ExceptionHandler(ImageProcessingException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleImageProcessingException(ImageProcessingException ex) {
@@ -28,6 +45,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+    /**
+     * Manipulador para exceções do tipo InvalidCredentialsException.
+     * 
+     * @param ex Exceção lançada quando as credenciais fornecidas são inválidas.
+     * @return Resposta com status 401 (Unauthorized) e mensagem de erro.
+     */
     @ExceptionHandler(InvalidCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex) {
@@ -35,6 +58,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
+    /**
+     * Manipulador para exceções genéricas.
+     * 
+     * @param ex Exceção genérica lançada em casos não tratados.
+     * @return Resposta com status 500 (Internal Server Error) e mensagem de erro padrão.
+     */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
