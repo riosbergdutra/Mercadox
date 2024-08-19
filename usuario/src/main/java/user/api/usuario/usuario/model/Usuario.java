333e11fa784705bfb5179d2ceecb15742e5014fd
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-19 16:25:41 -0300

    fazendo relacao entre usuario e endereco

--- usuario/src/main/java/user/api/usuario/usuario/model/Usuario.java
+++ usuario/src/main/java/user/api/usuario/usuario/model/Usuario.java
@@ -3,11 +3,13 @@ package user.api.usuario.usuario.model;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,6 +17,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -45,6 +48,9 @@ public class Usuario implements UserDetails {
 
     @Column(nullable = false, name = "senha")
     private String senha;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Endereco> enderecos;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, name = "role")
