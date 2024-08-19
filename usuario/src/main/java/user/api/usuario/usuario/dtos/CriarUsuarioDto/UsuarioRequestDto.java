375f4940e80e6949dacb8d21c2ae1e608abd7e48
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-19 16:30:56 -0300

    colocando endereco no request do usuario

--- usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
+++ usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
@@ -1,10 +1,13 @@
 package user.api.usuario.usuario.dtos.CriarUsuarioDto;
 
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import user.api.usuario.usuario.dtos.EnderecoDto.EnderecoDto;
 import user.api.usuario.usuario.enums.Role;
 
 public record UsuarioRequestDto(
@@ -12,5 +15,7 @@ public record UsuarioRequestDto(
         MultipartFile imagem,
         @NotBlank(message = "Email não pode estar vazio") @Email(message = "Email inválido") String email,
         @NotBlank(message = "Senha não pode estar vazia") String senha,
-        @NotNull(message = "Role não pode estar vazia") Role role) {
+        @NotNull(message = "Role não pode estar vazia") Role role,
+       @NotBlank(message = "algum dado faltando") List<EnderecoDto> enderecos
+        ) {
 }
