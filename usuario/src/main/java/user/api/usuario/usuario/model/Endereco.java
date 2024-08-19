3321c7bbc31f941109590780bf84d842c7c23132
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-19 16:25:15 -0300

    Feat: adicionando endereco

---
+++ usuario/src/main/java/user/api/usuario/usuario/model/Endereco.java
@@ -0,0 +1,48 @@
+package user.api.usuario.usuario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "enderecos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idEndereco;
+
+    @Column(nullable = false)
+    private String rua;
+
+    @Column(nullable = false)
+    private String numero;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false)
+    private String estado;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+}
