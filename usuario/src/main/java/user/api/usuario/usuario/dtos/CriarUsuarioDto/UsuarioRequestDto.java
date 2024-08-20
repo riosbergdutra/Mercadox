b56bf9d24bffe6fd3c59419e424626335418de10
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-20 13:32:54 -0300

    tirando o list do request do usuario

--- usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
+++ usuario/src/main/java/user/api/usuario/usuario/dtos/CriarUsuarioDto/UsuarioRequestDto.java
@@ -1,9 +1,10 @@
 package user.api.usuario.usuario.dtos.CriarUsuarioDto;
 
-import java.util.List;
+
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,11 +12,10 @@ import user.api.usuario.usuario.dtos.EnderecoDto.EnderecoDto;
 import user.api.usuario.usuario.enums.Role;
 
 public record UsuarioRequestDto(
-        @NotBlank(message = "Nome não pode estar vazio") String nome,
-        MultipartFile imagem,
-        @NotBlank(message = "Email não pode estar vazio") @Email(message = "Email inválido") String email,
-        @NotBlank(message = "Senha não pode estar vazia") String senha,
-        @NotNull(message = "Role não pode estar vazia") Role role,
-       @NotBlank(message = "algum dado faltando") List<EnderecoDto> enderecos
-        ) {
+                @NotBlank(message = "Nome não pode estar vazio") String nome,
+                MultipartFile imagem,
+                @NotBlank(message = "Email não pode estar vazio") @Email(message = "Email inválido") String email,
+                @NotBlank(message = "Senha não pode estar vazia") String senha,
+                @NotNull(message = "Role não pode estar vazia") Role role,
+                @Valid @NotNull EnderecoDto endereco) {
 }
