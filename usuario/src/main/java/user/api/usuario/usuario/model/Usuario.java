bea1db34260205144a4556e3427cb675b35c5010
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-20 13:28:20 -0300

    adicionando fetch no model de usuario

--- usuario/src/main/java/user/api/usuario/usuario/model/Usuario.java
+++ usuario/src/main/java/user/api/usuario/usuario/model/Usuario.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -49,7 +50,7 @@ public class Usuario implements UserDetails {
     @Column(nullable = false, name = "senha")
     private String senha;
 
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Endereco> enderecos;
 
     @Enumerated(EnumType.STRING)
