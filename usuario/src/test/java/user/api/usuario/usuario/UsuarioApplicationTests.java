d44cad5eaefec2de69555cb9e4b5ed101c1b2326
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-22 16:06:39 -0300

    habilitando properties de testes

--- usuario/src/test/java/user/api/usuario/usuario/UsuarioApplicationTests.java
+++ usuario/src/test/java/user/api/usuario/usuario/UsuarioApplicationTests.java
@@ -2,8 +2,10 @@ package user.api.usuario.usuario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UsuarioApplicationTests {
 
 	@Test
