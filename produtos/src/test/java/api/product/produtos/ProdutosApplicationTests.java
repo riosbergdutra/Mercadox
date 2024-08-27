d119c184765cc7f1058ccc58a95f9a9b99460bd2
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-27 15:41:40 -0300

    arquivos base para produto

---
+++ produtos/src/test/java/api/product/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package api.product.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
