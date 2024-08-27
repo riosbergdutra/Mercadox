024bb527074487b4dccf9b3150ee3ce48b3458ea
Riosberg Dutra <riosbergduttra@gmail.com>
2024-08-27 15:40:48 -0300

    enum para a categoria do produto

---
+++ produtos/src/main/java/api/product/produtos/enums/CategoriaProduto.java
@@ -0,0 +1,15 @@
+package api.product.produtos.enums;
+
+public enum CategoriaProduto {
+    ELETRONICOS,
+    ROUPA,
+    ALIMENTOS,
+    BEBIDAS,
+    MOVEIS,
+    ELETRODOMESTICOS,
+    JOGOS,
+    LIVROS,
+    SAUDE,
+    JOIAS,
+    MATERIAL_ESPORTIVO,
+}
