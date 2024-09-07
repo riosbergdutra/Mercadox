1be81174f408539cbe0a5492d4a781aa18333352
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-07 16:52:18 -0300

    getprodutobyid

--- produtos/src/main/java/api/product/produtos/service/impl/ProdutoServiceImpl.java
+++ produtos/src/main/java/api/product/produtos/service/impl/ProdutoServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import api.product.produtos.dtos.ProdutosDto.ProdutoDtoResponse;
+import api.product.produtos.dtos.produtobyidDto.ProdutoByIdResponse;
 import api.product.produtos.exceptions.FindAllProductsException;
+import api.product.produtos.exceptions.ProdutoNotFoundException;
 import api.product.produtos.model.Produto;
 import api.product.produtos.repository.ProdutoRepository;
 import api.product.produtos.service.ProdutoService;
@@ -19,26 +21,35 @@ public class ProdutoServiceImpl implements ProdutoService {
     private ProdutoRepository produtoRepository;
 
     @Override
-    public List<ProdutoDtoResponse> FindAllProducts() {
+    public List<ProdutoDtoResponse> getAllProdutos() {
         try {
             List<Produto> produtos = produtoRepository.findAll();
             return produtos.stream()
-                .map(this::convertToDto)
-                .collect(Collectors.toList());
+                    .map(produto -> new ProdutoDtoResponse(
+                            produto.getIdProduto(),
+                            produto.getNomeProduto(),
+                            produto.getUrlImagem(),
+                            produto.getPrecoProduto(),
+                            produto.getCategoriaProduto(),
+                            produto.getCidadeVendedor()))
+                    .collect(Collectors.toList());
         } catch (Exception e) {
             throw new FindAllProductsException("falha ao achar os produtos", e);
         }
     }
-    private ProdutoDtoResponse convertToDto(Produto produto) {
-        return new ProdutoDtoResponse(
-            produto.getIdProduto(),
-            produto.getNomeProduto(),
-            produto.getUrlImagem(),
-            produto.getDescricao(),
-            produto.getPrecoProduto(),
-            produto.getCategoriaProduto(),
-            produto.getCidadeVendedor()
-        );
+
+    @Override
+    public ProdutoByIdResponse getProdutoById(Long id) {
+        return produtoRepository.findById(id)
+                .map(produto -> new ProdutoByIdResponse(
+                        produto.getIdProduto(),
+                        produto.getNomeProduto(),
+                        produto.getUrlImagem(),
+                        produto.getDescricao(),
+                        produto.getPrecoProduto(),
+                        produto.getCategoriaProduto(),
+                        produto.getCidadeVendedor()))
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto não encontrado"));
     }
 
 }
