bcb90af3988b3cfb673501996f857397b8c15b62
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-07 16:51:44 -0300

    mudando nome e colocando novo metodo para achar o produto expecifico

--- produtos/src/main/java/api/product/produtos/service/ProdutoService.java
+++ produtos/src/main/java/api/product/produtos/service/ProdutoService.java
@@ -3,9 +3,10 @@ package api.product.produtos.service;
 import java.util.List;
 
 import api.product.produtos.dtos.ProdutosDto.ProdutoDtoResponse;
+import api.product.produtos.dtos.produtobyidDto.ProdutoByIdResponse;
 
 public interface ProdutoService {
 
-     List<ProdutoDtoResponse> FindAllProducts();
-
+     public List<ProdutoDtoResponse> getAllProdutos();
+     public ProdutoByIdResponse getProdutoById(Long id);
 }
