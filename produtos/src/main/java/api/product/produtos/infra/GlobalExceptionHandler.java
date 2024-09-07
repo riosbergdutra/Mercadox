f5e6d30f99b4c69fa875dda7f4304a189cc33fd5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-07 16:50:22 -0300

    colocando-o no global

--- produtos/src/main/java/api/product/produtos/infra/GlobalExceptionHandler.java
+++ produtos/src/main/java/api/product/produtos/infra/GlobalExceptionHandler.java
@@ -6,15 +6,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import api.product.produtos.exceptions.FindAllProductsException;
+import api.product.produtos.exceptions.ProdutoNotFoundException;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProdutoNotFoundException.class)
+    public ResponseEntity<String> handleProdutoNotFoundException(ProdutoNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
 
     @ExceptionHandler(FindAllProductsException.class)
     public ResponseEntity<String> handleProductServiceException(FindAllProductsException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    // Você pode adicionar outros manipuladores de exceção aqui
-}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ocorreu um erro interno. Tente novamente mais tarde.");
+    }}
