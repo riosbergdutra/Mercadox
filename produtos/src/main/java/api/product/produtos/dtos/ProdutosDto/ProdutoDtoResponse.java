e4228f1a774a9184dfd3bc03210af7028e1c43c3
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-07 16:49:40 -0300

    tirando descricao do produtodtoresponse

--- produtos/src/main/java/api/product/produtos/dtos/ProdutosDto/ProdutoDtoResponse.java
+++ produtos/src/main/java/api/product/produtos/dtos/ProdutosDto/ProdutoDtoResponse.java
@@ -1,15 +1,13 @@
 package api.product.produtos.dtos.ProdutosDto;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 import api.product.produtos.enums.CategoriaProduto;
 
 public record ProdutoDtoResponse(
-UUID idProduto,
+Long idProduto,
 String nomeProduto,
 String UrlImagem,
-Long descricao,
 BigDecimal PrecoProduto,
 CategoriaProduto categoriaProduto,
 String cidadeVendedor
