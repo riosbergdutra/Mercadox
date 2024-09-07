b41adeffa51edfbfb13a109c6955d38d11a4d5f8
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-07 16:51:03 -0300

    tirando do uuid para long

--- produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
+++ produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
@@ -1,10 +1,9 @@
 package api.product.produtos.repository;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import api.product.produtos.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
