057391a0bf1be27f90d31a4622d1fd103d8b0b8b
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-13 17:25:00 -0300

    adicinando um findbyvendedor para o repository

--- produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
+++ produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
@@ -1,9 +1,13 @@
 package api.product.produtos.repository;
 
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import api.product.produtos.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Produto> findByIdVendedor(UUID idVendedor);
 }
