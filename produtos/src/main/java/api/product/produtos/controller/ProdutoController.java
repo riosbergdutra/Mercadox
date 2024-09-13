cb3ce38847d5014ef7cd97edb5207fc603047089
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-13 17:28:14 -0300

    metodo privado para pegar o id do token e controlador para adicionar produto a conta do usuario

--- produtos/src/main/java/api/product/produtos/controller/ProdutoController.java
+++ produtos/src/main/java/api/product/produtos/controller/ProdutoController.java
@@ -1,18 +1,24 @@
 package api.product.produtos.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.product.produtos.dtos.ProdutosDto.ProdutoDtoRequest;
 import api.product.produtos.dtos.ProdutosDto.ProdutoDtoResponse;
 import api.product.produtos.dtos.produtobyidDto.ProdutoByIdResponse;
 import api.product.produtos.service.ProdutoService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -20,6 +26,16 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+    
+    @PostMapping("/{idVendedor}/criarproduto")
+    public ResponseEntity<ProdutoDtoResponse> addProduto(
+            @PathVariable("idVendedor") UUID idVendedor,
+            @ModelAttribute @Valid ProdutoDtoRequest produtoDtoRequest,
+            Authentication authentication) {
+        UUID userId = getUserIdFromAuthentication(authentication);
+        ProdutoDtoResponse resposta = produtoService.addProduto(produtoDtoRequest, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
 
     @GetMapping("/findall")
     public ResponseEntity<List<ProdutoDtoResponse>> getAllProdutos() {
@@ -31,5 +47,9 @@ public class ProdutoController {
     public ResponseEntity<ProdutoByIdResponse> getProdutoById(@PathVariable Long id) {
         ProdutoByIdResponse produto = produtoService.getProdutoById(id);
         return new ResponseEntity<>(produto, HttpStatus.OK);
+    }
+
+    private UUID getUserIdFromAuthentication(Authentication authentication) {
+        return UUID.fromString(authentication.getName());
     }
 }
