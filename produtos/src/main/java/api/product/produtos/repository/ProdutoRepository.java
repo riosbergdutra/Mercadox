bc6d6e9e66c2287ebddb71f8670f65a01126b346
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-16 13:37:16 -0300

    achar id do vendedor e o id do produto

--- produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
+++ produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
@@ -1,13 +1,14 @@
 package api.product.produtos.repository;
 
-
 import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import api.product.produtos.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     Optional<Produto> findByIdVendedor(UUID idVendedor);
+
+    Optional<Produto> findByIdVendedorAndIdProduto(UUID userId, Long idProduto);
+
 }
