2d2f6153fa3b5f82f2522c41e13e552fc14ed78e
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-24 16:56:55 -0300

    model do carrinho

---
+++ carrinho/src/main/java/cart/api/carrinho/model/Carrinho.java
@@ -0,0 +1,25 @@
+package cart.api.carrinho.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "carrinho")
+@Data
+public class Carrinho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // ou UUID se suportado
+    @Column(unique = true, nullable = false, name = "id_carrinho")
+    private UUID idCarrinho;
+
+    @Column(unique = true, nullable = false, name = "id_usuario")
+    private UUID idUsuario;
+    
+    @ElementCollection
+    @CollectionTable(name = "item_carrinho", joinColumns = @JoinColumn(name = "id_carrinho"))
+    private List<ItemCarrinho> itens = new ArrayList<>();
+}
