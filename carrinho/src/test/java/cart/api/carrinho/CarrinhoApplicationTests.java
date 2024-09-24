38ed3999e8849ce2172c18c24ff16af7c1dd8589
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-24 16:56:22 -0300

    arquivo base de teste

---
+++ carrinho/src/test/java/cart/api/carrinho/CarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package cart.api.carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
