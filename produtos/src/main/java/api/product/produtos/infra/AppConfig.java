050d39ecd0a4f7977a1394272ebfc6c115e74171
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-24 16:54:15 -0300

    resttemplate config para produto

---
+++ produtos/src/main/java/api/product/produtos/infra/AppConfig.java
@@ -0,0 +1,13 @@
+package api.product.produtos.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
