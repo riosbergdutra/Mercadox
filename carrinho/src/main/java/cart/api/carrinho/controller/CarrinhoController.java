0b883c903c0c3071add49d67a42a1eb676c9fb70
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-25 16:21:30 -0300

    controller para carrinho

---
+++ carrinho/src/main/java/cart/api/carrinho/controller/CarrinhoController.java
@@ -0,0 +1,42 @@
+package cart.api.carrinho.controller;
+
+import java.util.UUID;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import cart.api.carrinho.model.Carrinho;
+import cart.api.carrinho.service.CarrinhoService;
+
+import org.springframework.security.core.Authentication;
+
+public class CarrinhoController {
+     @Autowired
+    private CarrinhoService carrinhoService;
+
+    /**
+     * Endpoint para obter o carrinho de um usuário.
+     *
+     * @param idUsuario      ID do usuário a ser consultado.
+     * @param authentication Objeto de autenticação contendo o ID do usuário logado.
+     * @return ResponseEntity com o carrinho do usuário ou uma mensagem de erro.
+     */
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<?> getCarrinhoByIdUsuario(
+            @PathVariable UUID idUsuario,
+            Authentication authentication) {
+
+        UUID userId = UUID.fromString(authentication.getName());
+
+        Optional<Carrinho> carrinho = carrinhoService.getCarrinhoByIdUsuario(idUsuario, userId);
+
+        if (carrinho.isPresent()) {
+            return ResponseEntity.ok(carrinho.get());
+        } else {
+            return ResponseEntity.status(404).body("Carrinho não encontrado ou não autorizado.");
+        }
+    }
+}
