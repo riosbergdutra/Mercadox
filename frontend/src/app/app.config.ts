55020e83ef4d889005f0f075e561cd0935882c5b
Riosberg Dutra <riosbergduttra@gmail.com>
2024-09-30 16:54:38 -0300

    arquivos bases para o frontend angular

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+    provideHttpClient(withFetch()),
+    ReactiveFormsModule,
+  ]
+};
