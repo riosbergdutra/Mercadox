3f9950a550cad2e7c13082e9583ea6241a5a4277
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-03 15:53:25 -0300

    colocando a rota notfound no frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundComponent } from './notfound/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-
+    { path: '**', component: NotFoundComponent }
 
 ];
