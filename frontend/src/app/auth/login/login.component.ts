d5ed44ad5af1875084839e2eb4e6cdb452231638
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-03 15:54:46 -0300

    logicas para login

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService, LoginRequest } from '../auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';  // Import CommonModule
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule], // Adicione ReactiveFormsModule aqui
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: boolean = false;
 
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    // Inicializando o formulário
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      const loginRequest: LoginRequest = this.loginForm.value;
+
+      this.authService.login(loginRequest).subscribe({
+        next: () => {
+          // Redireciona para a página principal ou dashboard
+          this.router.navigate(['/']);
+        },
+        error: () => {
+          // Exibe uma mensagem de erro se o login falhar
+          this.loginError = true;
+        }
+      });
+    }
+  }
 }
