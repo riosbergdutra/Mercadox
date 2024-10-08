482be5f788a3f45d4e02b8e4e39a9860ace52d68
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-08 02:31:06 -0300

    adicinando o guard em uam rota para testes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '**', component: NotFoundComponent }
+    { path: '**', component: NotFoundComponent, canActivate: [authGuard] }
 
 ];
