e7e64dac01d27507c3983cc886b71cc755d66470
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-08 02:34:36 -0300

    colocando logica para o loading no componente principal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './loading/loading.component';
+import { LoadingService } from './loading/loading.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private loadingService: LoadingService) {}
+
+  get loading$() {
+    return this.loadingService.loading$;
+  }
+
 }
