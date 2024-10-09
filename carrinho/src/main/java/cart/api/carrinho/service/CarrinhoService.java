b7f5805d8cd98d8e7f2f5411a8697fd04e2da8f9
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-09 18:13:49 -0300

    tirando o optional do metodo getcarrinhobyidusuario

--- carrinho/src/main/java/cart/api/carrinho/service/CarrinhoService.java
+++ carrinho/src/main/java/cart/api/carrinho/service/CarrinhoService.java
@@ -1,11 +1,10 @@
 package cart.api.carrinho.service;
 
 import java.util.UUID;
-import java.util.Optional;
 
 import cart.api.carrinho.model.Carrinho;
 
 public interface CarrinhoService {
-    public Optional<Carrinho> getCarrinhoByIdUsuario(UUID idUsuario, UUID userId);
+    public Carrinho getCarrinhoByIdUsuario(UUID idUsuario, UUID userId);
     public void processarMensagemSQS(String mensagemSQS);
 }
