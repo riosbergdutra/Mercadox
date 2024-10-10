c89391c2deef32344fe0f9f80e3e381f944941f5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-10 17:15:37 -0300

    adicionar produto ao carrinho service

--- carrinho/src/main/java/cart/api/carrinho/service/CarrinhoService.java
+++ carrinho/src/main/java/cart/api/carrinho/service/CarrinhoService.java
@@ -2,9 +2,11 @@ package cart.api.carrinho.service;
 
 import java.util.UUID;
 
+import cart.api.carrinho.dto.CarrinhoDtoRequest;
 import cart.api.carrinho.model.Carrinho;
 
 public interface CarrinhoService {
     public Carrinho getCarrinhoByIdUsuario(UUID idUsuario, UUID userId);
     public void processarMensagemSQS(String mensagemSQS);
+    public void adicionarProdutoAoCarrinho(CarrinhoDtoRequest carrinhoDtoRequest,UUID idCarrinho, UUID idUsuario, UUID userId);
 }
