64170ee8be21a18215da660b5d11a40f670e9061
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-14 01:34:48 -0300

    adicionando preco unitario e valor

--- carrinho/src/main/java/cart/api/carrinho/model/ItemCarrinho.java
+++ carrinho/src/main/java/cart/api/carrinho/model/ItemCarrinho.java
@@ -3,11 +3,19 @@ package cart.api.carrinho.model;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
+import java.math.BigDecimal;
 
 @Embeddable
 @Data
 public class ItemCarrinho {
      private Long idProduto;
      @Min(1)
-     private Integer quantidade;
+     private int quantidade;
+
+     private BigDecimal precoUnitario;
+
+
+     public BigDecimal getValorTotalItem() {
+          return this.precoUnitario.multiply(BigDecimal.valueOf(this.quantidade)); 
+     }
 } 
