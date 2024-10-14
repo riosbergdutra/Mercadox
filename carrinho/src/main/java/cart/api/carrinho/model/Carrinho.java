529b639ed782e05ebdaa7e586cd0bd1be18018ad
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-14 01:35:25 -0300

    valor total e logica para valor total do carrinho

--- carrinho/src/main/java/cart/api/carrinho/model/Carrinho.java
+++ carrinho/src/main/java/cart/api/carrinho/model/Carrinho.java
@@ -1,10 +1,10 @@
 package cart.api.carrinho.model;
 
-
 import java.util.UUID;
 import java.util.List;
 
 import jakarta.persistence.*;
+import java.math.BigDecimal;
 
 import lombok.Data;
 
@@ -19,8 +19,18 @@ public class Carrinho {
 
     @Column(unique = true, nullable = false, name = "id_usuario")
     private UUID idUsuario;
-    
+
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "item_carrinho", joinColumns = @JoinColumn(name = "id_carrinho"))
     private List<ItemCarrinho> itens;
+
+    @Column(name = "valor_total", nullable = false)
+    private BigDecimal valorTotal = BigDecimal.ZERO;
+
+    public void calcularValorTotal() {
+        this.valorTotal = itens.stream()
+                .map(ItemCarrinho::getValorTotalItem) 
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
 }
