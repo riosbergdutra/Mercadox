51a6af0f7e63cdf18b18a25b6a50fc77190cbcd1
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-22 14:49:44 -0300

    config para swagger no auth

---
+++ auth/src/main/java/auth/api/auth/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package auth.api.auth.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("api-publica-auth")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
