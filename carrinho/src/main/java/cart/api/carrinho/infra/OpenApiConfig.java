b68bc272be960370446a5de47c02673a35a0898e
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-22 14:58:12 -0300

    config para swagger no carrinho

---
+++ carrinho/src/main/java/cart/api/carrinho/infra/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package cart.api.carrinho.infra;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+     @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("api-publica-auth")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
