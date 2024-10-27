144bfe2287247abe0e037a868383faf220d630f9
Riosberg Dutra <riosbergduttra@gmail.com>
2024-10-27 16:03:58 -0300

    metodo de finalizar pedido

--- carrinho/src/main/java/cart/api/carrinho/service/CarrinhoService.java
+++ carrinho/src/main/java/cart/api/carrinho/service/CarrinhoService.java
@@ -3,12 +3,14 @@ package cart.api.carrinho.service;
 import java.util.UUID;
 
 import cart.api.carrinho.dto.CarrinhoDtoRequest;
+import cart.api.carrinho.dto.CriarPedidoRequestDto;
 import cart.api.carrinho.model.Carrinho;
 
 public interface CarrinhoService {
-    public Carrinho getCarrinhoByIdUsuario(UUID idUsuario, UUID userId);
+    public Carrinho getCarrinhoByIdUsuario(UUID idUsuario);
     public void processarMensagemSQS(String mensagemSQS);
-    public void adicionarProdutoAoCarrinho(CarrinhoDtoRequest carrinhoDtoRequest,UUID idCarrinho, UUID idUsuario, UUID userId);
-    public void limparCarrinho(UUID idCarrinho, UUID idUsuario, UUID userId);
-    public void removerItemDoCarrinho(UUID idCarrinho, UUID idUsuario, Long idProduto, UUID userId);
+    public void adicionarProdutoAoCarrinho(CarrinhoDtoRequest carrinhoDtoRequest,UUID idCarrinho, UUID idUsuario);
+    public void limparCarrinho(UUID idCarrinho, UUID idUsuario);
+    public void removerItemDoCarrinho(UUID idCarrinho, UUID idUsuario, Long idProduto);
+    public void finalizarPedido(UUID idUsuario, UUID idCarrinho, CriarPedidoRequestDto pedidoRequestDto);
 }
