fc9025262552be6c93889d51d9ecba35c9bea77e
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-03 14:42:24 -0300

    find by id por usuario e pelo pedido e usuario no checkout

--- checkout/src/main/java/pedido/api/checkout/repository/PedidoRepository.java
+++ checkout/src/main/java/pedido/api/checkout/repository/PedidoRepository.java
@@ -1,11 +1,13 @@
 package pedido.api.checkout.repository;
 
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pedido.api.checkout.model.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, UUID>{
-    
+  Optional<Pedido>findByIdPedidoAndIdUsuario(UUID idPedido, UUID idUsuario);
+  Optional<Pedido>findByIdUsuario(UUID idUsuario);
 }
