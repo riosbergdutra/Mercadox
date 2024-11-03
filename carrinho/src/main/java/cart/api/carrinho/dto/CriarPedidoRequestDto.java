acb0841567aa4351d69d454d4e47e08401428e25
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-03 14:46:32 -0300

    tirando id do usuario do dto do criar pedido do carrinho

--- carrinho/src/main/java/cart/api/carrinho/dto/CriarPedidoRequestDto.java
+++ carrinho/src/main/java/cart/api/carrinho/dto/CriarPedidoRequestDto.java
@@ -4,12 +4,10 @@ package cart.api.carrinho.dto;
 import cart.api.carrinho.enums.FormaPagamento;
 import cart.api.carrinho.model.ItemCarrinho;
 
-import java.util.UUID;
 import java.math.BigDecimal;
 import java.util.List;
 
 public record CriarPedidoRequestDto(
-    UUID idUsuario,
     Endereco enderecoEntrega,
     List<ItemCarrinho> itens, 
     BigDecimal valorCompra,
