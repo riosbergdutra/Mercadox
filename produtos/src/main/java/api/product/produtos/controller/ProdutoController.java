8b10d44f302dd456088d1e1e74f14a9f797f01ff
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-08 15:45:47 -0300

    comentarios para controller

--- produtos/src/main/java/api/product/produtos/controller/ProdutoController.java
+++ produtos/src/main/java/api/product/produtos/controller/ProdutoController.java
@@ -27,6 +27,11 @@ import api.product.produtos.model.Produto;
 import api.product.produtos.service.ProdutoService;
 import jakarta.validation.Valid;
 
+/**
+ * Controlador para gerenciar operações relacionadas aos produtos.
+ * 
+ * Fornece endpoints para criar, atualizar, excluir e consultar produtos.
+ */
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
@@ -34,7 +39,15 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
-    // Atualizar produto
+    /**
+     * Endpoint para atualizar as informações de um produto existente.
+     * 
+     * @param idVendedor      ID do vendedor que está atualizando o produto.
+     * @param idProduto       ID do produto a ser atualizado.
+     * @param produtoDtoRequest DTO contendo os dados atualizados do produto.
+     * @param authentication  Objeto de autenticação para obter o ID do usuário logado.
+     * @return ResponseEntity com o DTO do produto atualizado e status 200 (OK).
+     */
     @PutMapping("/{idVendedor}/atualizarproduto/{idProduto}")
     public ResponseEntity<ProdutoDtoResponse> updateProduto(
             @PathVariable("idVendedor") UUID idVendedor,
@@ -48,7 +61,14 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.OK).body(produtoAtualizado);
     }
 
-    // Remover produto
+    /**
+     * Endpoint para excluir um produto.
+     * 
+     * @param idVendedor     ID do vendedor que está deletando o produto.
+     * @param idProduto      ID do produto a ser excluído.
+     * @param authentication Objeto de autenticação para verificar o ID do usuário logado.
+     * @return ResponseEntity com status 204 (NO CONTENT) após a exclusão do produto.
+     */
     @DeleteMapping("/{idVendedor}/deletarproduto/{idProduto}")
     public ResponseEntity<Void> deleteProduto(
             @PathVariable("idVendedor") UUID idVendedor,
@@ -61,6 +81,14 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    /**
+     * Endpoint para criar um novo produto.
+     * 
+     * @param idVendedor      ID do vendedor que está criando o produto.
+     * @param produtoDtoRequest DTO contendo os dados do produto a ser criado.
+     * @param authentication  Objeto de autenticação para obter o ID do usuário logado.
+     * @return ResponseEntity com o DTO do produto criado e status 201 (CREATED).
+     */
     @PostMapping("/{idVendedor}/criarproduto")
     public ResponseEntity<ProdutoDtoResponse> addProduto(
             @PathVariable("idVendedor") UUID idVendedor,
@@ -71,25 +99,47 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
     }
 
+    /**
+     * Endpoint para obter todos os produtos cadastrados.
+     * 
+     * @return ResponseEntity com a lista de produtos e status 200 (OK).
+     */
     @GetMapping("/findall")
     public ResponseEntity<List<ProdutoDtoResponse>> getAllProdutos() {
         List<ProdutoDtoResponse> produtos = produtoService.getAllProdutos();
         return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
-    
+
+    /**
+     * Endpoint para obter os produtos mais comprados.
+     * 
+     * @return ResponseEntity com a lista dos produtos mais comprados e status 200 (OK).
+     */
     @GetMapping("/maiscomprados")
-public ResponseEntity<List<ProdutoDtoResponse>> getTop10ProdutosMaisComprados() {
-    List<ProdutoDtoResponse> produtos = produtoService.getTop10ProdutosMaisComprados();
-    return ResponseEntity.ok(produtos);
-}
+    public ResponseEntity<List<ProdutoDtoResponse>> getTop10ProdutosMaisComprados() {
+        List<ProdutoDtoResponse> produtos = produtoService.getTop10ProdutosMaisComprados();
+        return ResponseEntity.ok(produtos);
+    }
 
-
+    /**
+     * Endpoint para obter um produto específico pelo seu ID.
+     * 
+     * @param id ID do produto a ser consultado.
+     * @return ResponseEntity com o DTO do produto e status 200 (OK).
+     */
     @GetMapping("/{id}")
     public ResponseEntity<ProdutoByIdResponse> getProdutoById(@PathVariable Long id) {
         ProdutoByIdResponse produto = produtoService.getProdutoById(id);
         return new ResponseEntity<>(produto, HttpStatus.OK);
     }
 
+    /**
+     * Endpoint para verificar se há estoque suficiente para um produto específico.
+     * 
+     * @param idProduto ID do produto a ser consultado.
+     * @param quantidade Quantidade de produtos a ser verificada.
+     * @return ResponseEntity com o status de estoque (true ou false) e status 200 (OK).
+     */
     @GetMapping("/{idProduto}/verificar-estoque/{quantidade}")
     public ResponseEntity<Boolean> verificarEstoqueDisponivel(
             @PathVariable Long idProduto,
@@ -99,6 +149,15 @@ public ResponseEntity<List<ProdutoDtoResponse>> getTop10ProdutosMaisComprados() 
         return ResponseEntity.ok(temEstoqueSuficiente);
     }
 
+    /**
+     * Endpoint para adicionar um produto ao carrinho de um usuário.
+     * 
+     * @param idCarrinho ID do carrinho do usuário.
+     * @param idUsuario  ID do usuário que está adicionando o produto.
+     * @param requestDto Dados do produto a ser adicionado ao carrinho.
+     * @param authentication Objeto de autenticação contendo o ID do usuário logado.
+     * @return ResponseEntity com status 204 (NO CONTENT) após adicionar o produto ao carrinho.
+     */
     @PostMapping("/{idCarrinho}/adicionar/{idUsuario}")
     public ResponseEntity<Void> adicionarProdutoAoCarrinho(
             @PathVariable("idCarrinho") UUID idCarrinho,
@@ -119,6 +178,15 @@ public ResponseEntity<List<ProdutoDtoResponse>> getTop10ProdutosMaisComprados() 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    /**
+     * Endpoint para atualizar o estoque de um produto.
+     * 
+     * @param idVendedor   ID do vendedor que está atualizando o estoque.
+     * @param idProduto    ID do produto cujo estoque será atualizado.
+     * @param request      DTO com a quantidade a ser adicionada ao estoque.
+     * @param authentication Objeto de autenticação para verificar o ID do usuário logado.
+     * @return ResponseEntity com o produto atualizado e status 200 (OK).
+     */
     @PostMapping("/vendedores/{idVendedor}/produtos/{idProduto}/estoque")
     public ResponseEntity<Produto> atualizarEstoque(
             @PathVariable UUID idVendedor,
@@ -128,21 +196,32 @@ public ResponseEntity<List<ProdutoDtoResponse>> getTop10ProdutosMaisComprados() 
                 
         UUID userId = getUserIdFromAuthentication(authentication);
 
-        Produto produtoAtualizado = produtoService.adicionarEstoque(userId, idProduto,
-                request.quantidade());
+        Produto produtoAtualizado = produtoService.adicionarEstoque(userId, idProduto, request.quantidade());
         return ResponseEntity.ok(produtoAtualizado);
     }
 
+    /**
+     * Endpoint para registrar a compra de um produto.
+     * 
+     * @param idProduto ID do produto que foi comprado.
+     * @param quantidade Quantidade do produto comprada.
+     * @return ResponseEntity com o produto atualizado após a compra e status 200 (OK).
+     */
     @PostMapping("/{idProduto}/registrar-compra")
-public ResponseEntity<Produto> registrarCompra(
-        @PathVariable Long idProduto,
-        @RequestBody int quantidade) {
+    public ResponseEntity<Produto> registrarCompra(
+            @PathVariable Long idProduto,
+            @RequestBody int quantidade) {
 
-    Produto produtoAtualizado = produtoService.registrarCompra(idProduto, quantidade );
-    return ResponseEntity.ok(produtoAtualizado);
-}
+        Produto produtoAtualizado = produtoService.registrarCompra(idProduto, quantidade);
+        return ResponseEntity.ok(produtoAtualizado);
+    }
 
-
+    /**
+     * Método auxiliar para obter o ID do usuário a partir do objeto de autenticação.
+     * 
+     * @param authentication Objeto de autenticação contendo os dados do usuário logado.
+     * @return O ID do usuário logado como UUID.
+     */
     private UUID getUserIdFromAuthentication(Authentication authentication) {
         return UUID.fromString(authentication.getName());
     }
