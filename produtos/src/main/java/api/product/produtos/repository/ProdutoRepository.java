427b641d223ae30c99fb2f8fc4f4d621e0a9d727
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-08 15:38:04 -0300

    findall para ver os produtos mais comprados

--- produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
+++ produtos/src/main/java/api/product/produtos/repository/ProdutoRepository.java
@@ -2,6 +2,9 @@ package api.product.produtos.repository;
 
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import api.product.produtos.model.Produto;
@@ -10,5 +13,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     Optional<Produto> findByIdVendedor(UUID idVendedor);
 
     Optional<Produto> findByIdVendedorAndIdProduto(UUID userId, Long idProduto);
+
+    Page<Produto> findAll(Pageable pageable);
+
 
 }
