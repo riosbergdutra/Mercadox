ab5cbe85c05f362632246e20ed8f60692d7f2802
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-12 12:04:07 -0300

    rota para homepage e minha conta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { authGuard } from './auth/auth.guard';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MinhaContaComponent } from './minha-conta/minha-conta.component';
 
 export const routes: Routes = [
+    {path:"", component: HomepageComponent},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '**', component: NotFoundComponent, canActivate: [authGuard] }
+    {path:"minha-conta", component: MinhaContaComponent},  
+    { path: '**', component: NotFoundComponent}
 
 ];
