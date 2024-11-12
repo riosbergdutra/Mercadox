b368c6aad7ee3b74d7944e2c9dae0e16ed9f4adf
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-12 12:03:03 -0300

    component para minha conta

---
+++ frontend/src/app/minha-conta/minha-conta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minha-conta',
+  standalone: true,
+  imports: [],
+  templateUrl: './minha-conta.component.html',
+  styleUrl: './minha-conta.component.css'
+})
+export class MinhaContaComponent {
+
+}
