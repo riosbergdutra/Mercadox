cf8889473cfe5d950a389378cb75fc4af374db31
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-16 16:04:25 -0300

    imports e logicas para o carrinho e navegacao para pedido

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -2,19 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import {MatBadgeModule} from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatBadgeModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  cartItemCount = 0;
   isModalOpen = false;
   isAuthenticated: boolean = false;
 
   constructor(private authService: AuthService,  private router: Router) {}
+
+  addToCart() {
+    this.cartItemCount++;
+  }
 
   ngOnInit(): void {
     // Verificar se o usuário está logado ao inicializar o componente
@@ -39,4 +49,13 @@ export class HeaderComponent {
     // Navegar para a página de login
     this.router.navigateByUrl('/login');
   }
+
+  // Redirecionar para a página de pedidos ou login
+  goToOrders() {
+    if (this.isAuthenticated) {
+      this.router.navigateByUrl('/pedidos'); // Página de pedidos
+    } else {
+      this.router.navigateByUrl('/login');  // Página de login
+    }
+}
 }
