ed62e398b6aba176786271d76f5ed029f78766fb
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-18 18:20:17 -0300

    correcao para verificar se o usuario esta autenticado no header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -4,6 +4,9 @@ import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import {MatBadgeModule} from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
+import { Observable, of, take } from 'rxjs';
+import { Endereco } from '../models/Endereco';
+import { EnderecoService } from '../enderecoService/endereco.service';
 
 
 
@@ -18,18 +21,44 @@ import { MatIconModule } from '@angular/material/icon';
 export class HeaderComponent {
   cartItemCount = 0;
   isModalOpen = false;
-  isAuthenticated: boolean = false;
-
-  constructor(private authService: AuthService,  private router: Router) {}
+  enderecos: Endereco[] = [];
+  userId: string = '';  
+  isAuthenticated$: Observable<boolean>;
+  constructor(private authService: AuthService,  private router: Router, private enderecoService: EnderecoService) {
+    this.isAuthenticated$ = this.authService.isAuthenticated();
+  }
 
   addToCart() {
     this.cartItemCount++;
   }
 
   ngOnInit(): void {
-    // Verificar se o usuário está logado ao inicializar o componente
-    this.isAuthenticated = this.authService.isAuthenticated();
+    // Verificar se o usuário está autenticado e carregar o ID
+    this.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        // Busca o ID do usuário do token JWT
+        this.userId = this.authService.getUserId() ?? '';
+        if (this.userId) {
+          this.loadEnderecos();
+        }
+      }
+    });
   }
+// Método para carregar os endereços do usuário
+  // Método para carregar os endereços do usuário
+  loadEnderecos(): void {
+    if (this.userId) {
+      this.enderecoService.getEnderecos(this.userId).subscribe({
+        next: (enderecos) => {
+          this.enderecos = enderecos;
+        },
+        error: (err) => {
+          console.error('Erro ao carregar endereços', err);
+        }
+      });
+    }
+  }
+
 
   openModal() {
     this.isModalOpen = true;
@@ -52,10 +81,12 @@ export class HeaderComponent {
 
   // Redirecionar para a página de pedidos ou login
   goToOrders() {
-    if (this.isAuthenticated) {
-      this.router.navigateByUrl('/pedidos'); // Página de pedidos
-    } else {
-      this.router.navigateByUrl('/login');  // Página de login
-    }
+    this.isAuthenticated$.pipe(take(1)).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('/pedidos'); // Página de pedidos
+      } else {
+        this.router.navigateByUrl('/login'); // Página de login
+      }
+    });
 }
 }
