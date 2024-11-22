73d2a08e1a6a1d608eb25e9a73927d5a30a8a7d2
Riosberg Dutra <riosbergduttra@gmail.com>
2024-11-22 13:40:40 -0300

    mudanca de logica e pagina do register

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,96 +1,46 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService, UsuarioRequestDto } from '../auth.service';
-import { CommonModule } from '@angular/common'; // Adicione esta linha
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router'; // Importando o Router
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule], // Adicione o CommonModule aqui
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'] // Corrigido de 'styleUrl' para 'styleUrls'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   registerForm: FormGroup;
-  isStepOne: boolean = true;
-  isVendedor: boolean = false;
+  registerError: boolean = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService,
+    private router: Router // Injete o Router
+  ) {
     this.registerForm = this.fb.group({
       nome: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       senha: ['', Validators.required],
       role: ['', Validators.required],
-      endereco: this.fb.group({
-        rua: ['', Validators.required],
-        numero: ['', Validators.required],
-        cidade: ['', Validators.required],
-        estado: ['', Validators.required],
-        cep: ['', Validators.required],
-      }),
-      imagem: [null],
     });
-  }
-
-  onRoleChange(): void {
-    const selectedRole = this.registerForm.get('role')?.value;
-    this.isVendedor = selectedRole === 'VENDEDOR';
-  }
-
-  nextStep(): void {
-    if (this.registerForm.get('nome')?.valid && 
-        this.registerForm.get('email')?.valid && 
-        this.registerForm.get('senha')?.valid && 
-        this.registerForm.get('role')?.valid) {
-      this.isStepOne = false;
-    }
-  }
-
-  prevStep(): void {
-    this.isStepOne = true; // Retorna para a primeira etapa
   }
 
   onSubmit() {
     if (this.registerForm.valid) {
-      const formData = new FormData();
-  
-      // Adiciona os campos do formulário
-      formData.append('nome', this.registerForm.get('nome')?.value);
-      formData.append('email', this.registerForm.get('email')?.value);
-      formData.append('senha', this.registerForm.get('senha')?.value);
-      formData.append('role', this.registerForm.get('role')?.value);
-  
-      // Adiciona os campos do endereço
-      const endereco = this.registerForm.get('endereco');
-      if (endereco) {
-        formData.append('endereco.rua', endereco.get('rua')?.value);
-        formData.append('endereco.numero', endereco.get('numero')?.value);
-        formData.append('endereco.cidade', endereco.get('cidade')?.value);
-        formData.append('endereco.estado', endereco.get('estado')?.value);
-        formData.append('endereco.cep', endereco.get('cep')?.value);
-      }
-  
-      // Adiciona a imagem se existir
-      const imagem = this.registerForm.get('imagem')?.value;
-      if (imagem) {
-        formData.append('imagem', imagem);
-      }
-  
-      this.authService.register(formData).subscribe({
+      const usuarioRequest: UsuarioRequestDto = this.registerForm.value;
+
+      this.authService.register(usuarioRequest).subscribe({
         next: () => {
           console.log('Usuário registrado com sucesso!');
+          this.router.navigate(['/login']); 
         },
-        error: (err) => {
-          console.error('Erro ao registrar usuário:', err);
+        error: () => {
+          this.registerError = true;
         },
       });
     }
-  }
-  
-  
-
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    this.registerForm.patchValue({ imagem: file });
   }
 }
