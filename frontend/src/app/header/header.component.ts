d6e8e1e97d495ae5b7a0d3e05da4da79bb6df805
Riosberg Dutra <riosbergduttra@gmail.com>
2024-12-01 14:44:59 -0300

    corrigindo logica do header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
-import {MatBadgeModule} from '@angular/material/badge';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable, of, take } from 'rxjs';
 import { Endereco } from '../models/Endereco';
 import { EnderecoService } from '../services/enderecoService/endereco.service';
-
-
-
 
 @Component({
   selector: 'app-header',
@@ -18,55 +15,52 @@ import { EnderecoService } from '../services/enderecoService/endereco.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   cartItemCount = 0;
   isModalOpen = false;
   enderecos: Endereco[] = [];
-  userId$: Observable<string | null>;
+  idUsuario$: Observable<string | null>;
   isAuthenticated$: Observable<boolean>;
-  constructor(private authService: AuthService,  private router: Router, private enderecoService: EnderecoService) {
-    this.isAuthenticated$ = this.authService.isAuthenticated();
-    this.userId$ = this.authService.getUserId()
-  }
 
-  addToCart() {
-    this.cartItemCount++;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private enderecoService: EnderecoService
+  ) {
+    this.isAuthenticated$ = this.authService.authenticated$;
+    this.idUsuario$ = this.authService.getUserId();
   }
 
   ngOnInit(): void {
-    // Verificar se o usuário está autenticado e carregar o ID
-    this.isAuthenticated$.subscribe((isAuthenticated) => {
+    console.log('[HeaderComponent] Inicialização do componente.');
+    this.isAuthenticated$.pipe(take(1)).subscribe((isAuthenticated) => {
+      console.log(`[HeaderComponent] Usuário autenticado: ${isAuthenticated}`);
       if (isAuthenticated) {
-        // Busca o ID do usuário do token JWT e atribui à variável userId$
-        this.userId$ = this.authService.getUserId();
-      }
-    });
-  }
-  loadEnderecos(): void {
-    this.authService.getUserId().pipe(take(1)).subscribe({
-      next: (userId) => {
-        if (userId) {
-          this.enderecoService.getEnderecos(userId).subscribe({
-            next: (enderecos) => {
-              this.enderecos = enderecos;
-            },
-            error: (err) => {
-              console.error('Erro ao carregar endereços', err);
-            }
-          });
-        } else {
-          console.error('User ID não encontrado.');
-        }
-      },
-      error: (err) => {
-        console.error('Erro ao obter User ID', err);
+        this.loadEnderecos();
       }
     });
   }
   
-
-
-
+  private loadEnderecos(): void {
+    console.log('[HeaderComponent] Carregando endereços...');
+    this.idUsuario$.pipe(take(1)).subscribe((idUsuario) => {
+      console.log(`[HeaderComponent] User ID: ${idUsuario}`);
+      if (idUsuario) {
+        this.enderecoService.getEnderecos(idUsuario).subscribe({
+          next: (enderecos) => {
+            console.log('[HeaderComponent] Endereços carregados:', enderecos);
+            this.enderecos = enderecos;
+          },
+          error: (err) => {
+            console.error('[HeaderComponent] Erro ao carregar endereços:', err);
+          },
+        });
+      } else {
+        console.warn('[HeaderComponent] User ID não encontrado.');
+      }
+    });
+  }
+  
   openModal() {
     this.isModalOpen = true;
   }
@@ -76,24 +70,20 @@ export class HeaderComponent {
   }
 
   goToAccount() {
-    // Navegar para a página "Minha Conta"
-    this.router.navigateByUrl('/minha-conta')
+    this.router.navigateByUrl('/minha-conta');
   }
 
-  // Redirecionar para a página de login
   goToLogin() {
-    // Navegar para a página de login
     this.router.navigateByUrl('/login');
   }
 
-  // Redirecionar para a página de pedidos ou login
   goToOrders() {
     this.isAuthenticated$.pipe(take(1)).subscribe((isAuthenticated) => {
       if (isAuthenticated) {
-        this.router.navigateByUrl('/pedidos'); // Página de pedidos
+        this.router.navigateByUrl('/pedidos');
       } else {
-        this.router.navigateByUrl('/login'); // Página de login
+        this.router.navigateByUrl('/login');
       }
     });
-}
+  }
 }
